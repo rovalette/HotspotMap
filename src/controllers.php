8fcc7427485cd9c0ce099606f7d435a92438c53c
Romain Valette <rom.valette@gmail.com>
2014-02-26 16:46:16 +0100

    some refactoring

--- src/controllers.php
+++ src/controllers.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 //Request::setTrustedProxies(array('127.0.0.1'));
 
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html', array());
+    return $app['twig']->render('index.twig', array());
 })
 ->bind('homepage')
 ;
@@ -19,12 +19,12 @@ $app->error(function (\Exception $e, $code) use ($app) {
         return;
     }
 
-    // 404.html, or 40x.html, or 4xx.html, or error.html
+    // 404.twig, or 40x.twig, or 4xx.twig, or error.twig
     $templates = array(
-        'errors/'.$code.'.html',
-        'errors/'.substr($code, 0, 2).'x.html',
-        'errors/'.substr($code, 0, 1).'xx.html',
-        'errors/default.html',
+        'errors/'.$code.'.twig',
+        'errors/'.substr($code, 0, 2).'x.twig',
+        'errors/'.substr($code, 0, 1).'xx.twig',
+        'errors/default.twig',
     );
 
     return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
